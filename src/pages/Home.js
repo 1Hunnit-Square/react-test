deafc263ef6994f57452f547159c1d3054a965b9
baek <bsm100100@gmail.com>
2025-04-04 17:33:14 +0900

    edit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,28 @@
 import Button from "../component/Button";
 import Header from "../component/Header";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../App";
+import { getMonthRangeByDate } from "../util"
 
 const Home = () => {
+    const data = useContext(DiaryStateContext);
     const [pivotDate, setPivotDate ] =useState(new Date());
+    const [filteredData, setFilteredData] = useState([]);
     const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() +1}월`;
+
+    useEffect(() => {
+        if(data.length >= 1){
+            const {beginTimeStamp, endTimeStamp} = getMonthRangeByDate(pivotDate);
+            setFilteredData(
+                data.filter(
+                    (it) => beginTimeStamp <= it.date && it.date <= endTimeStamp
+                )
+            )
+        } else {
+            setFilteredData([]);
+        }
+    }, [data, pivotDate]);
+
 
     const onIncreaseMonth = () => {
         setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1 ));
--- src/util.js
+++ src/util.js
@@ -64,3 +64,17 @@ export const emotionList = [
 }
 
 ]
+
+
+export const getMonthRageByDate = (date) => {
+    const beginTimeStamp = new Date(date.getFullYear(), date.getMonth(), 1).getTime();
+    const endTimeStamp = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0,
+        23,
+        59,
+        59
+    ).getTime();
+    return { beginTimeStamp, endTimeStamp }
+}
